5c3d147c6806c3c2aa4a42eac04396786c5c2df0
ananastasiap <anestheziia@gmail.com>
2024-03-09 16:52:21 +0300

    feat: слайдер готов, нужна стилизация кнопок + добавить контент

--- src/components/Recommendations/Slider.tsx
+++ src/components/Recommendations/Slider.tsx
@@ -1,53 +1,43 @@
-// import { useState } from "react";
+import { useRef } from "react";
 import { slides } from "./types";
 import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore from "swiper";
 import "swiper/css";
 
 const Slider: React.FC = () => {
-  // const [currentSlide, setCurrentSlide] = useState(0);
+  const swiperRef = useRef<SwiperCore | null>(null);
 
-  // const handlePrev = () => {
-  //   if (currentSlide > 0) {
-  //     setCurrentSlide(currentSlide - 1);
-  //   }
-  // };
+  const handlePrev = () => {
+    swiperRef.current?.slidePrev();
+  };
 
-  // const handleNext = () => {
-  //   if (currentSlide < slides.length - 1) {
-  //     setCurrentSlide(currentSlide + 1);
-  //   }
-  // };
+  const handleNext = () => {
+    swiperRef.current?.slideNext();
+  };
 
   return (
-    <Swiper
-      spaceBetween={14}
-      slidesPerView={2}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
-      className="line slider-lit"
-    >
-      {slides.map((slide) => (
-        <SwiperSlide className="slider-card" key={slide.digit}>
-          <span className="slider-card--digit">{`${slide.digit}/`}</span>
-          <h4 className="slider-card--title">{slide.title}</h4>
-          <p className="slider-card--desc">{slide.desc}</p>
-        </SwiperSlide>
-      ))}
-    </Swiper>
-
-    // <div className="line slider-lit">
-    // <button onClick={handlePrev}>prev</button>
-    // <button onClick={handleNext}>next</button>
-    //   <div className="slider">
-    // {slides.map((slide) => (
-    //   <div className="slider-card">
-    //     <span className="slider-card--digit">{`${slide.digit}/`}</span>
-    //     <h4 className="slider-card--title">{slide.title}</h4>
-    //     <p className="slider-card--desc">{slide.desc}</p>
-    //   </div>
-    // ))}
-    //   </div>
-    // </div>
+    <div className="line slider-lit">
+      <button onClick={handlePrev}>Prev</button>
+      <button onClick={handleNext}>Next</button>
+      <Swiper
+        spaceBetween={14}
+        slidesPerView={2}
+        loop
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => {
+          swiperRef.current = swiper;
+        }}
+        navigation={true}
+      >
+        {slides.map((slide) => (
+          <SwiperSlide className="slider-card" key={slide.digit}>
+            <span className="slider-card--digit">{`${slide.digit}/`}</span>
+            <h4 className="slider-card--title">{slide.title}</h4>
+            <p className="slider-card--desc">{slide.desc}</p>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   );
 };
 
