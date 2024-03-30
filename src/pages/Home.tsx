3ff9915dfeb96b3d84edaf14a2449765102d743b
Nadezhda Ilina <116672516+naniylid@users.noreply.github.com>
2024-03-30 09:25:30 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from '../components/Navigation/Navigation';
 import Header from '../components/Header/Header';
 import StudyPlan from '../components/StudyPlan';
+import Recommendations from '../components/Recommendations';
 import numbers from '../assets/image/numbers.svg';
 
 const Home: React.FC = () => {
@@ -11,6 +12,7 @@ const Home: React.FC = () => {
       <img className='numbers' src={numbers} alt='' />
       <Header />
       <StudyPlan />
+      <Recommendations/>
     </>
   );
 };
